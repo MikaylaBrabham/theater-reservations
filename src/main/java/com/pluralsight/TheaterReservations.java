1de014d61fbf13b88e3e8b3214d4cc52a3ea366d
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-04-27 03:27:30 -0500

    add both date types options

--- src/main/java/com/pluralsight/TheaterReservations.java
+++ src/main/java/com/pluralsight/TheaterReservations.java
@@ -5,8 +5,6 @@ import java.time.format.DateTimeFormatter;
 //import scanner
 import java.util.Scanner;
 
-//import date formatter
-
 
 public class TheaterReservations {
 
@@ -15,7 +13,7 @@ public class TheaterReservations {
         Scanner scanner = new Scanner(System.in);
 
         //prompt for users name
-        System.out.println("Please enter your name: ");
+        System.out.printf("Please enter your name: ");
         String fullName = scanner.nextLine();
 
         //prompt for users show date
@@ -30,6 +28,19 @@ public class TheaterReservations {
         //prompt for users amount tickets
         System.out.printf("How many tickets would you like? ");
         Integer txAmount = scanner.nextInt();
+
+        //display confirmation  message if over 1 ticket amount
+        if(txAmount > 1) {
+            String confirmationMessage;
+            System.out.printf("%d tickets reserved for %s under %s"
+                    , txAmount, formattedShowDate, fullName);
+        }
+        //display confirmation message if only 1 ticket
+        if(txAmount == 1) {
+            String  confirmationMessageTwo;
+            System.out.printf("%d ticket reserved for %s under %s"
+                    , txAmount, formattedShowDate, fullName);
+        }
 
 
     }
